6218e49b2f622f116ea705cb149cb91bc3aaffd6
Project <andersonmartins@yahoo.com.br>
2020-09-22 22:16:22 -0300

    Project Created

---
+++ src/main/java/com/webserv/jpa/hibernate/WebservSprigbootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.webserv.jpa.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservSprigbootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservSprigbootJpaHibernateApplication.class, args);
+	}
+
+}
